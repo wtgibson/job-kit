d96731d1ffb5bda58ad8ceead66a8e7aa030c32c
nadineb <nadineb@umich.edu>
2020-04-26 15:42:04 -0700

    [add] company model and associations

--- models/company.js
+++ models/company.js
@@ -1,1 +1,28 @@
-module.exports
+module.exports = function (sequelize, DataTypes) {
+    var Company = sequelize.define("Company", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        zipCode: {
+            type: DataTypes.STRING
+        },
+        URL: {
+            type: DataTypes.STRING
+        }
+        
+
+    });
+
+    Company.associate = models => {
+        models.Company.hasMany(models.Contact, {foreignkey: 'id'}, {
+            onDelete: "cascade"
+        });
+        models.Company.hasMany(models.Application, {foreignkey: 'id'}, {
+            onDelete: "cascade"
+        });
+
+    }
+    return Company;
+}
+
