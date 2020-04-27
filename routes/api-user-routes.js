a14a025163121c7aa0baa42bd082ea10ac0b2745
ChinDowns <webdev@chindowns.com>
2020-04-27 13:16:14 -0700

    schema, seeders and more api routes

--- routes/api-user-routes.js
+++ routes/api-user-routes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
             where: req.body
         }).then(user=> {
             // send user id back to client
-            res.json(user.id);
+            res.send(user.id);
         }).catch(err => {
             // error
             console.log(err);
