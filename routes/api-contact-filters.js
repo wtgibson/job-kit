29e063eeb27814972e9279633a064520d0e09085
nadineb <nadineb@umich.edu>
2020-04-28 13:22:38 -0700

    [add] contact filters file

---
+++ routes/api-contact-filters.js
@@ -0,0 +1,20 @@
+var db = require("../models");
+
+module.exports = function (app) {
+
+
+    // Get All Contacts by Type
+    // app.get("/api/contacts/filter/type/:typeName", (req, res) => {
+    //     db.Contacts.findAll({
+    //         where: {
+    //             CompanyId: req.params.companyId
+    //         },
+    //     }).then(contacts => {
+    //         res.json(contacts);
+    //     }).catch(err => {
+    //         console.log(err);
+    //         res.send(false);
+    //     });  
+    // });
+
+}
