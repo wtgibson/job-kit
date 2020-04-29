ee61387da784b371fbaae9db2d0263109b2820e5
nadineb <nadineb@umich.edu>
2020-04-28 17:48:54 -0700

    [add] include models, get using field, dataValues

--- routes/api-company-routes.js
+++ routes/api-company-routes.js
@@ -7,7 +7,9 @@ module.exports = function (app) {
         db.Company.findAll({
             where: {
                 UserId: req.params.userId
-            }
+            },
+            include:
+                [db.Contact]
         }).then(companies => {
             res.json(companies);
         }).catch(err => {
@@ -19,11 +21,13 @@ module.exports = function (app) {
     // Get Company using ID
     app.get("/api/user/:userId/company/:companyName", (req, res) => {
         db.Company.findAll({
-            where: 
+            where:
             {
                 UserId: req.params.userId,
                 name: req.params.companyName
-            }
+            },
+            include:
+                [db.Contact]
         }).then(company => {
             res.json(company);
         }).catch(err => {
@@ -32,14 +36,31 @@ module.exports = function (app) {
         });
     });
 
+    // Get All Company - Name, Zipcode
+    app.get("/api/user/:userId/company/all/:field", (req, res) => {
+        db.Company.findAll({
+            where: {
+                UserId: req.params.userId
+            },
+            attributes: [req.params.field]
+        }).then(companies => {
+            res.json(companies);
+            console.log(companies);
+        }).catch(err => {
+            console.log(err);
+            res.send("No data found");
+        });
+    });
+
     // Create Company 
     app.post("/api/company", (req, res) => {
         db.Company.create(req.body, {
         }).then(company => {
-            res.send(`Company, ${company.name}, has been created`)
+            console.log(company)
+            res.send(`Company, ${company.dataValues.name}, has been created`)
         }).catch(err => {
             console.log(err);
-            res.send(`Company, ${company.name}, was NOT created`)
+            res.send(`Company, ${company.dataValues.name}, was NOT created`)
         });
     });
 
