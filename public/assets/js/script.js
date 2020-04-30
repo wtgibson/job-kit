173bc325b408f0cd5a02d8c319c3a133ac32ad31
Ana Medrano <amedrano.fernandez@gmail.com>
2020-04-29 18:56:33 -0700

    updated jobs form so that job posting will take user to the post and updated brackets on description so that html is properly rendered.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,33 +4,23 @@ $(function () {
     var defaultJob;
     var defaultName;
 
-    // loads user data for default values
-
-    // $.ajax("/api/user/"+globalUserId, {
-    //     type: "GET",
-    // }).then(function (data) {
-    //     defaultZipCode = data.zipCode;
-    //     defaultJob = data.jobTile;
-    //     defaultName = data.name;
-    // })
-
-    // function for saving job data
 
     $(document).on("click", ".link-to-ext", function () {
-        event.preventDefault();
+        // event.preventDefault();
         var id = $(this).data("jobid");
         var title = $(`#title-${id}`).text();
         var desc = $(`#desc-${id}`).text();
 
-
         var newApp = {
             title: title,
+            type: "",
             description: "desc",
             industry: "None",
             zipCode: "94114",
             salaryRange: "0",
             rating: 0,
-            UserId: globalUserId
+            dataApplied: Date.now(),
+            UserId: globalUserID
         }
 
         $.ajax("/api/application", {
@@ -38,18 +28,7 @@ $(function () {
             data: newApp,
         }).then(function (res) {
             console.log(res)
-            // var newCompany = {
-            //     name: $(`#company-${id}`).text(),
-            //     zipCode: "None",
-            //     url: $(`#company-${id}`).attr("href"),
-            //     UserID: globalUserId
-            // }
-            // $.ajax("/api/company", {
-            //     type: "POST",
-            //     data: newCompany
-            // }).then(function () {
-            //     console.log("Successfully added the company")
-            // })
+            
         })
 
     })
