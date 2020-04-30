d7c725eff41f17eab99924da2e970e52ea2087cf
ChinDowns <webdev@chindowns.com>
2020-04-30 15:40:22 -0700

    Bug Fix authenticate.js on click

--- public/assets/js/authenticatate.js
+++ public/assets/js/authenticatate.js
@@ -14,7 +14,7 @@ firebase.initializeApp(firebaseConfig);
 // var database = firebase.database();
 
 // event listener for login screen
-$("#login-button").on("submit", function (event) {
+$("#login-button").on("click", function (event) {
   event.preventDefault();
   var email = $("#email");
   var password = $("#password");
@@ -75,16 +75,6 @@ $("#login-button").on("submit", function (event) {
       })
     }
 
-      function getfireBaseUser() {
-      var user = firebase.auth().currentUser;
-      var name, email;
-      
-      if (user != null) {
-        name = user.displayName;
-        email = user.email;
-        return email;
-      }
-
     // Event listener from Firebase that checks with user auth state changes
     firebase.auth().onAuthStateChanged(function (user) {
       // if user is authenticated then...
@@ -128,5 +118,5 @@ $("#login-button").on("submit", function (event) {
       // sign out of firebase
       firebase.auth().signOut();
     })
-  }
+  
   });
