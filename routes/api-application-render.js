307d3819e8f3ba6f0ae13f978360ef649a354557
nadineb <nadineb@umich.edu>
2020-05-02 15:52:15 -0700

    [change] comment out companyName

--- routes/api-application-render.js
+++ routes/api-application-render.js
@@ -20,7 +20,7 @@ module.exports = function renderApplications(applications, res, partial) {
         rating: element.dataValues.rating,
         // createdAt: element.dataValues.createdAt,
         // updatedAt: element.dataValues.updatesAt,
-        companyName: element.dataValues.Company.dataValues.name,
+        // companyName: element.dataValues.Company.dataValues.name,
         companyObj: element.dataValues.Company.dataValues,
         contactObj: element.dataValues.Contacts,
         stageObj: element.dataValues.Stages,
